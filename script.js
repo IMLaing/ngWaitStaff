614af038720472ed964ec9d94fed33c595dd4e0e
IMLaing <ianmlaing@gmail.com>
2016-10-30 15:45:39 -0400

    added in animations

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngMessages', 'ngRoute']);
+var myApp = angular.module('myApp', ['ngMessages', 'ngRoute', 'ngAnimate']);
 
 myApp.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
